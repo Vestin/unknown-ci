2c0d6bda5f76386210adc7cb5b0b7b7b37d08894
vestin <bombzds>
2017-07-11 18:21:23 +0800

    project create & edit

--- laravel/app/Http/Controllers/ProjectController.php
+++ laravel/app/Http/Controllers/ProjectController.php
@@ -4,8 +4,10 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Project;
+use Illuminate\Support\Facades\Session;
 use Illuminate\Support\Facades\View;
-use TwigBridge\Facade\Twig;
+use Illuminate\Validation\Rule;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class ProjectController extends Controller
 {
@@ -13,12 +15,63 @@ class ProjectController extends Controller
     public function all()
     {
         $projects = Project::all();
-        return View::make('projects',['projects'=>$projects]);
+        return View::make('projects', ['projects' => $projects]);
     }
 
-    public function detail($id){
+    public function detail($id)
+    {
         $project = Project::find($id);
-        return View::make('project',['project'=>$project]);
+        dd(Session::get('status'));
+        return View::make('project', ['project' => $project]);
+    }
+
+    public function add(Request $request)
+    {
+        if($request->isMethod('get')){
+            return View::make('add-project');
+        }
+
+        $this->validate($request,[
+            'name' => 'required|unique:projects|max:255',
+            'owner' => 'required|min:1',
+        ]);
+
+        $project = new Project();
+        $project->name = $request->name;
+        $project->owner = $request->owner;
+        $project->yml = $request->yml;
+        $project->save();
+        $request->session()->flash('status','Task was successful!');
+
+        return response()->redirectToRoute('project',[$project->id]);
+    }
+
+    public function edit(Request $request,$id)
+    {
+        $project = Project::find($id);
+        if(!$project){
+            throw new NotFoundHttpException();
+        }
+
+        if($request->isMethod('get')){
+            return View::make('edit-project',['project'=>$project]);
+        }
+
+        $this->validate($request,[
+            'name' => [
+                'required',
+                Rule::unique('projects')->ignore($project->id)
+            ],
+            'owner' => 'required|min:1',
+        ]);
+
+        $project->name = $request->name;
+        $project->owner = $request->owner;
+        $project->yml = $request->yml;
+        $project->save();
+        $request->session()->flash('status','Task was successful!');
+
+        return response()->redirectToRoute('project',[$project->id]);
     }
 
 }
--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -12,11 +12,17 @@
 */
 
 use Illuminate\Support\Facades\View;
-
+use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     //return view::make('hello');
     return view('welcome');
 });
 
+//show projects
 Route::get('projects','ProjectController@all')->name('projects');
+//project detail page
 Route::get('project/{id}','ProjectController@detail')->name('project');
+//add project page
+Route::match(['get','post'],'project','ProjectController@add')->name('add-project');
+//updatde project page
+Route::match(['get','post'],'project/edit/{id}','ProjectController@edit')->name('edit-project');
